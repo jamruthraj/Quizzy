9f1e81eef41c80d61dfd193436120f6979925526
Amruth Raja Josyula <jamruthraj@gmail.com>
2023-05-19 09:07:40 +0530

    [Completed]

--- client.py
+++ client.py
@@ -1,45 +1,71 @@
 import rpyc
 
+score = 0
+
 
 class QuizClient:
+    # initialize the client
+    def __init__(self):
+        self.conn = rpyc.connect("localhost", 12351)
+        self.category = None
 
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
+    # start the quiz and get the player name
+    def start_quiz(self):
+        name = input("Enter your name: ")
+        player_id = self.conn.root.register()
+        if player_id != -1:
+            print(f"{name}, you are registered as player {player_id}")
+            return (name, player_id)
+        else:
+            print("Sorry, the quiz is full.")
+            return None
 
-    def start(self):
-        conn = rpyc.connect("localhost", 12347)
+    # select the category
+    def select_category(self):
+        categories = self.conn.root.getcategories()
+        print("Available categories:")
+        for i, category in enumerate(categories):
+            print(f"{i + 1}. {category}")
+        choice = input("Enter the number of the category you want to select: ")
+        try:
+            choice = int(choice)
+            if choice < 1 or choice > len(categories):
+                raise ValueError
+        except ValueError:
+            print("Invalid choice.")
+            return self.select_category()
+        self.category = categories[choice - 1]
+        return self.category
 
-        print(f"Welcome {self.name} to the Quiz Game!")
-        print("Select a category to start:")
-        print("1. Sports")
-        print("2. History")
-        print("3. General Knowledge")
+    # get the question and validate the answer
+    def play_game(self, name, player_id):
+        global score
+        if not self.category:
+            print("No category selected.")
+            return
 
-        category = input("Enter your choice: ")
-        while category not in ['1', '2', '3']:
-            print("Invalid choice. Please try again.")
-            category = input("Enter your choice: ")
-
-        category_map = {'1': 'sports', '2': 'history', '3': 'gk'}
-        questions = conn.root.get_questions(category_map[category])
-
-        for q in questions:
-            print(q)
-            answer = input("Your answer: ")
-            answer = answer.strip().lower()
-            result = conn.root.validate_answer(q, answer)
-            if result:
-                print("Correct!")
-                self.score += 1
+        print(f"{name}, the category is {self.category}")
+        questions = self.conn.root.getquestions(self.category)
+        for i, (q, a) in enumerate(questions.items()):
+            print(f"\nQuestion {i+1}: {q}")
+            answer = input("Answer: ")
+            if self.conn.root.validateanswer(answer, name, q, self.category):
+                print("Correct answer!")
+                score += 1
             else:
-                print("Incorrect.")
+                print("Wrong answer.")
 
-        winner = conn.root.get_winner(self.name, self.score)
-        print(f"The winner is: {winner}")
+    # print the winner
+    def get_winner(self):
+        global score
+        print("Your Score: ", score)
+        print(self.conn.root.getwinner(name, score))
 
 
-if __name__ == "__main__":
-    name = input("Enter your name: ")
-    client = QuizClient(name)
-    client.start()
+if __name__ == '__main__':
+    client = QuizClient()
+    name, player_id = client.start_quiz()
+    if name:
+        client.select_category()
+        client.play_game(name, player_id)
+        client.get_winner()
--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 import rpyc
-import random
+import time
+
+started = 0
+finished = 0
+top = -1
+winners = []
 
 
 class QuizService(rpyc.Service):
@@ -8,7 +13,7 @@ class QuizService(rpyc.Service):
         self.scores = {}
         self.questions = {
             'sports': {
-                'Which year did India win its first Hockey World Cup?': 1975,
+                'Which year did India win its first Hockey World Cup?': '1975',
                 'First Indian Bowler to take Hatrick in an International Test Match?': 'harbhajansingh',
                 'Oldest Tennis Tournament?': 'wimbledon',
                 'Which sport does LeBron James play?': 'basketball',
@@ -29,46 +34,68 @@ class QuizService(rpyc.Service):
                 'Who discovered Electron': 'jjthompson'
             }
         }
-        self.current_question = None
         self.current_category = None
-        self.answer = None
+        self.players = []
+        self.client_ids = []
+
+    # get the list of categories
+    def exposed_getcategories(self):
+        return list(self.questions.keys())
 
     # get the question for the given category
-    def exposed_get_question(self, category):
-        questions = self.questions[category]
-        question = random.choice(list(questions.keys()))
-        answer = questions[question]
-        self.current_question = question
+    def exposed_getquestions(self, category):
+        currques = self.questions[category]
         self.current_category = category
-        self.answer = answer
-        return self.current_question
+        return currques
 
     # validate the answer and add the score
-    def exposed_validate_answer(self, answer, name):
-        if answer.lower().replace(" ", "") == str(self.answer).lower().replace(" ", ""):
+    def exposed_validateanswer(self, answer, name, q, category):
+        ans = self.questions[category][q]
+        if answer.lower().replace(" ", "") == ans:
             if name in self.scores:
                 self.scores[name] += 1
             else:
                 self.scores[name] = 1
             return True
         else:
+            self.scores[name] = 0
             return False
 
-    # get the winner
-    def exposed_get_winner(self):
-        if len(self.scores) == 0:
-            return "No winner yet. All players have quit."
-        max_score = max(self.scores.values())
-        winners = [name for name, score in self.scores.items()
-                   if score == max_score]
-        if len(winners) == 1:
-            return f"The winner is {winners[0]} with a score of {max_score}."
+    # register player and get player name
+    def exposed_register(self):
+        global started
+        if started < 4:
+            started += 1
+            client_id = started
+            self.client_ids.append(started)
+            print(f"Client {client_id} connected.")
+            return client_id
         else:
-            return f"It's a tie between {', '.join(winners)} with a score of {max_score} each."
+            return -1
+
+    # get the winner
+    def exposed_getwinner(self, name, score):
+        global finished, top, winners
+        if score > top:
+            winners = []
+            winners.append(name)
+            top = score
+        if score == top:
+            winners.append(name)
+        finished += 1
+        while finished < 4:
+            time.sleep(2)
+
+        winners = set(winners)
+        winners = list(winners)
+        if len(winners) == 1:
+            return f"The winner is {winners[0]} with a score of {top}."
+        else:
+            return f"It's a tie between {', '.join(winners)} with a score of {top} each."
 
 
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
-    t = ThreadedServer(QuizService, port=12347,
+    t = ThreadedServer(QuizService, port=12351,
                        protocol_config={"allow_public_attrs": True})
     t.start()
